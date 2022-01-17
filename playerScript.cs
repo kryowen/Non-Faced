83b10a7f2fd48d0ec22c77af8ebc3294c54e7caf
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-01-17 13:29:54 +0900

    Add files via upload

---
+++ playerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : Status {
+
+    //Status status;
+
+    //private void Awake() {
+    //    status = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Status>();
+    //}
+    // 다른 스크립트에서 이러한 형식으로 컴포넌트 접근
+
+    // Init
+    public playerScript() {
+        atk = 0;
+        atk_d = 0;
+        def = 0;
+        hp = 0;
+        mov = 0;
+        shield_stamina = 0;
+    }
+    
+    // 위치정보 추가
+    // 개성 추가
+}
+
+
+
+
