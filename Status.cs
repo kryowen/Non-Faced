aabf1dbb8dcaf55fe8db2511bec3ac479d0500c0
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-01-21 17:40:23 +0900

    주말에 추가 작업

---
+++ Status.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour {
+    protected int atk; // 공격력
+    protected float atk_d; // 공격딜레이
+    protected int brk; // 방어파괴?
+    protected int def; // 방어력
+    protected int max_hp; // 전체체력
+    protected int curr_hp; // 현재체력
+    protected int mov; // 이동속도
+    protected int shield_stamina; // 실드 스테미나
+
+    protected bool isDead; // 죽음?
+    protected bool isMoving; // 움직이는 중?
+
+    Vector2 position;
+
+    // Init
+    public Status() {
+        atk = 0;
+        brk = crash();
+        // 생성자에서 호출 한 뒤, 그 값을 그대로 사용
+        def = 0;
+        max_hp = 0;
+        curr_hp = 0;
+        mov = 0;
+
+        isDead = false;
+        isMoving = false;
+    }
+
+    private void FixedUpdate() {
+        if (curr_hp < 1) {
+            setDead(true);
+        }
+
+        if (Dead()) {
+            Debug.Log(name + "hp: " + curr_hp + ", Destroyed");
+            GameObject.Destroy(this);
+        }
+    }
+
+    /* 결과 >> 1    결과 >> 3
+     ATK = 20;      ATK = 75;
+     DEF = 10;      DEF = 30;
+     HP = 100;      HP = 1000;
+     MOV = 3;       MOV = 5;
+    */
+    protected int crash() {
+        return 1; //((2 * mov + (int)Mathf.Sqrt(atk)) % def) / 4;
+    } // 객체들의 세부적인 변수를 수정하면 그때 수식을 작성?
+
+    // get()
+    public int getAtk() {
+        return atk;
+    }
+
+    public float getAtk_d() {
+        return atk_d;
+    }
+
+    public int getBrk() {
+        return brk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public int getHp() {
+        return curr_hp;
+    }
+
+    public int getMaxHp() {
+        return max_hp;
+    }
+
+    public int getMov() {
+        return mov;
+    }
+
+    public int getShld_sta() {
+        return shield_stamina;
+    }
+
+    public bool Dead() { // 객체 사망?
+        return isDead;
+    }
+
+    public bool Move() { // 객체 움직이는 상태?
+        return isMoving;
+    }
+
+    // set()
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public void setAtk_d(float atk_d) {
+        this.atk_d = atk_d;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public void setHp(int hp) {
+        this.curr_hp = hp;
+    }
+
+    public void setMaxHp(int hp) {
+        this.max_hp = hp;
+    }
+
+    public void setMov(int mov) {
+        this.mov = mov;
+    }
+
+    public void setShield_stamina(int shield_stamina) {
+        this.shield_stamina = shield_stamina;
+    }
+
+    public void setDead(bool isDead) {
+        this.isDead = isDead;
+    }
+
+    public void setMove(bool isMoving) {
+        this.isMoving = isMoving;
+    }
+}
---
+++ playerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : Status {
+
+    //Status status;
+
+    //private void Awake() {
+    //    status = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Status>();
+    //}
+    // 다른 스크립트에서 이러한 형식으로 컴포넌트 접근
+
+
+    // 개성 추가
+    int obesity;
+    int moral;
+
+    // 위치 정보 추가
+    public Vector2 playerPos;
+
+    // 감각
+    public bool eyesLost;
+    public bool earsLost;
+    public bool mouthLost;
+
+    // Init
+    public playerScript() {
+        this.name = "Player";
+        atk = 0;
+        atk_d = 0;
+        def = 0;
+        max_hp = 0;
+        curr_hp = 0;
+        mov = 0;
+        shield_stamina = 0;
+
+        eyesLost = false;
+        earsLost = false;
+        mouthLost = false;
+        // 여기에 관한 처리들 석상과 대화에서 컴포넌트로 받아서 처리
+    }
+
+    private void Update() {
+        playerPos = this.gameObject.transform.position;
+    }
+}
